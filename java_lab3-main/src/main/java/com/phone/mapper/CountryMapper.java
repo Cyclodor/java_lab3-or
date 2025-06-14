3361a6923cbda2f7021c0166723251f46952bb0f
Cyclodor <Groza-1807@mail.ru>
2025-06-14 09:50:17 +0300

    ack

--- java_lab3-main/src/main/java/com/phone/mapper/CountryMapper.java
+++ java_lab3-main/src/main/java/com/phone/mapper/CountryMapper.java
@@ -20,7 +20,9 @@ public class CountryMapper {
 
     public CountryDtoResponse toDto(Country entity) {
         CountryDtoResponse dto = new CountryDtoResponse();
-        
+        dto.setCode(entity.getCode());
+        dto.setName(entity.getName());
+        dto.setPhoneCode(entity.getPhoneCode());
 
         List<PrefixDtoResponse> prefixDtos = entity.getPrefixes() != null
                 ? entity.getPrefixes().stream()
